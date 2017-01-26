f2927e9b8d8b7c77320d2d2668fe977baf2b5797
Conrad Kreyling <konistehrad@gmail.com>
2017-01-26 15:41:50 -0500

    test step needs SASS love too

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -114,7 +114,9 @@ module.exports = function (options) {
         {
           test: /\.scss$/,
           use: ['style-loader', 'css-loader', 'sass-loader'],
-          include: [helpers.root('src', 'styles')]
+          include: [
+            helpers.root('src', 'styles'),
+          ]
         },
 
       ]
