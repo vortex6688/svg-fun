be63fe8947be017b9c35a814ac500315cb98af0e
Darrel O'Pry <darrel.opry@spry-group.com>
2017-02-10 00:41:27 -0500

    import bootstrap into styleguide.

--- src/app/tn-styleguide/styleguide.component/component.ts
+++ src/app/tn-styleguide/styleguide.component/component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'styleguide',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: [ './style.scss'  ],
   templateUrl: './styleguide.component.html'
 })
