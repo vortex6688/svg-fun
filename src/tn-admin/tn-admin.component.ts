5e46b02966971506943c931bf61b6190885dd991
Darrel O'Pry <darrel.opry@spry-group.com>
2017-02-09 19:02:21 -0500

    remove vestigial items.

--- src/tn-admin/tn-admin.component.ts
+++ src/tn-admin/tn-admin.component.ts
@@ -23,34 +23,11 @@ import { AppState } from './tn-admin.service';
       <a [routerLink]=" ['./'] " routerLinkActive="active">
         Index
       </a>
-      <a [routerLink]=" ['./home'] " routerLinkActive="active">
-        Home
-      </a>
-      <a [routerLink]=" ['./detail'] " routerLinkActive="active">
-        Detail
-      </a>
-      <a [routerLink]=" ['./barrel'] " routerLinkActive="active">
-        Barrel
-      </a>
-      <a [routerLink]=" ['./about'] " routerLinkActive="active">
-        About
-      </a>
     </nav>
 
     <main>
       <router-outlet></router-outlet>
     </main>
-
-    <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
-
-    <footer>
-      <span>WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a></span>
-      <div>
-        <a [href]="url">
-          <img [src]="angularclassLogo" width="25%">
-        </a>
-      </div>
-    </footer>
   `
 })
 export class AppComponent implements OnInit {
