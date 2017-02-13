58fda78cd4305de5d0c5bddf69ffc069d5d20a52
Darrel O'Pry <darrel.opry@spry-group.com>
2017-02-13 18:01:34 -0500

    Styleguide App Module with initial view components.

---
+++ src/app/tn-styleguide/layout/layout-responsive-utilities/layout-responsive-utilities.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-responsive-utilities',
+  templateUrl: './layout-responsive-utilities.component.html',
+  styleUrls: ['./layout-responsive-utilities.component.css']
+})
+export class LayoutResponsiveUtilitiesComponent implements OnInit {
+
+  public ngOnInit() {
+    console.log('LayoutResponsiveUtilitiesComponent ngOnInit');
+  }
+}
