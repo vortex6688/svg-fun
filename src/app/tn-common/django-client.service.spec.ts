f4340c6897dfe10e607d712a104cd347b574bd8d
Carlos Chitty <chitty.carlosj@gmail.com>
2017-02-13 16:32:35 -0500

    3221 DjangoClient _setAuthToken, get, post, put and delete methods and tests

---
+++ src/app/tn-common/django-client.service.spec.ts
@@ -0,0 +1,145 @@
+/* tslint:disable:no-unused-variable */
+import {
+  BaseRequestOptions,
+  ConnectionBackend,
+  Http,
+  HttpModule,
+  RequestMethod,
+  Response,
+  ResponseOptions
+} from '@angular/http';
+import { TestBed, async, inject } from '@angular/core/testing';
+import { MockBackend } from '@angular/http/testing';
+import { DjangoClientService } from './django-client.service';
+
+describe('DjangoClientService', () => {
+  let djangoClientService;
+  let mockBackend;
+
+  const mockResponse = {
+    id: 88,
+    name: 'Jane Doe',
+    slug: 'Jane_Doe',
+    description: 'Lorem ipsum dolor',
+    birth_date: '1927-02-15',
+    death_date: '2017-02-16',
+    foundry: [2],
+    title: []
+  };
+
+  const data = {
+    full_bio: 'This a full bio.',
+    person: 88,
+    short_bio: 'Short bio.',
+    job_title: 'A job title.'
+  };
+
+  const url = 'http://localhost:8000/api/1/people/';
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpModule],
+      providers: [
+        DjangoClientService,
+        BaseRequestOptions,
+        MockBackend,
+        {
+          provide: Http,
+          useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
+            return new Http(backend, defaultOptions);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        }
+      ]
+    });
+  });
+
+  beforeEach(inject([DjangoClientService, MockBackend], (service, mock) => {
+    djangoClientService = service;
+    mockBackend = mock;
+  }));
+
+  it('should be created', () => {
+    expect(djangoClientService).toBeTruthy();
+  });
+
+  it('should fetch a single element by a given key', () => {
+    mockBackend.connections.subscribe((connection) => {
+      expect(connection.request.method).toBe(RequestMethod.Get, 'Not using GET method');
+      expect(connection.request.url).toBe(url, 'Incorrect URL used');
+      expect(connection.request.headers).toBeTruthy('No Headers');
+      expect(connection.request.headers.get('Authorization'))
+            .toBeTruthy('No Authorization field in Headers exist');
+      expect(connection.request.headers.get('Authorization').length)
+            .toBeGreaterThan(7, 'Content of Authorization is smaller than 7 characters');
+      connection.mockRespond(new Response(new ResponseOptions({
+        body: JSON.stringify(mockResponse)
+      })));
+    });
+
+    djangoClientService.get(url, {}).subscribe((result) => {
+      expect(result).toEqual(mockResponse, 'Response does not match');
+    });
+  });
+
+  it('should create an element with the given data', () => {
+    mockBackend.connections.subscribe((connection) => {
+      expect(connection.request.method).toBe(RequestMethod.Post, 'Not using POST method');
+      expect(connection.request.url).toBe(url, 'Incorrect URL used');
+      expect(connection.request._body).toBe(data, 'Incorrect data sent');
+      expect(connection.request.headers).toBeTruthy('No Headers');
+      expect(connection.request.headers.get('Authorization'))
+            .toBeTruthy('No Authorization field in Headers exist');
+      expect(connection.request.headers.get('Authorization').length)
+            .toBeGreaterThan(7, 'Content of Authorization is smaller than 7 characters');
+      connection.mockRespond(new Response(new ResponseOptions({
+        body: JSON.stringify(mockResponse)
+      })));
+    });
+
+    djangoClientService.post(url, data, {}).subscribe((result) => {
+      expect(result).toEqual(mockResponse, 'Response does not match');
+    });
+  });
+
+  it('should send data to update an element to the proper url using the PUT method', () => {
+    mockBackend.connections.subscribe((connection) => {
+      expect(connection.request.method).toBe(RequestMethod.Put, 'Not using PUT method');
+      expect(connection.request.url).toBe(url, 'Incorrect URL used');
+      expect(connection.request._body).toBe(data, 'Incorrect data sent');
+      expect(connection.request.headers).toBeTruthy('No Headers');
+      expect(connection.request.headers.get('Authorization'))
+            .toBeTruthy('No Authorization field in Headers exist');
+      expect(connection.request.headers.get('Authorization').length)
+            .toBeGreaterThan(7, 'Content of Authorization is smaller than 7 characters');
+      connection.mockRespond(new Response(new ResponseOptions({
+        body: 200
+      })));
+    });
+
+    djangoClientService.put(url, data, {}).subscribe((result, status) => {
+      expect(result).toEqual(200, 'Status is not 200');
+    });
+  });
+
+  it('should send data to delete an element to the proper url using the DELETE method', () => {
+    mockBackend.connections.subscribe((connection) => {
+      expect(connection.request.method).toBe(RequestMethod.Delete,
+                                             'Not using DELETE method');
+      expect(connection.request.url).toBe(url, 'Incorrect URL used');
+      expect(connection.request.headers).toBeTruthy('No Headers');
+      expect(connection.request.headers.get('Authorization'))
+            .toBeTruthy('No Authorization field in Headers exist');
+      expect(connection.request.headers.get('Authorization').length)
+            .toBeGreaterThan(7, 'Content of Authorization is smaller than 7 characters');
+      connection.mockRespond(new Response(new ResponseOptions({
+        body: 200
+      })));
+    });
+
+    djangoClientService.delete(url, {}).subscribe((result) => {
+      expect(result).toEqual(200, 'Status is not 200');
+    });
+  });
+
+});
---
+++ src/app/tn-common/django-client.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+
+@Injectable()
+export class DjangoClientService {
+
+  // @todo #3236: private user = userService.getUser();
+  private user = { token: 'ThisIsAToken' };
+
+  constructor(private https: Http) { }
+
+  public get(url: string, options: RequestOptions): Observable<any> {
+    let _options = this._setAuthToken(options);
+    return this.https.get(url, _options)
+                     .map((response) => response.json())
+                     .catch(this.handleError);
+  }
+
+  public post(url: string, data: Object, options: RequestOptions): Observable<any> {
+    let _options = this._setAuthToken(options);
+    return this.https.post(url, data, _options)
+                     .map((response) => response.json())
+                     .catch(this.handleError);
+  }
+
+  public put(url: string, data: Object, options: RequestOptions): Observable<any> {
+    let _options = this._setAuthToken(options);
+    return this.https.put(url, data, _options)
+                     .map((response) => response.json())
+                     .catch(this.handleError);
+  }
+
+  public delete(url: string, options: RequestOptions): Observable<any> {
+    let _options = this._setAuthToken(options);
+    return this.https.delete(url, _options)
+                     .map((response) => response.json())
+                     .catch(this.handleError);
+  }
+
+  private _setAuthToken(options: RequestOptions): RequestOptions {
+    // @todo #3236: this has to be a real token for django to allow access
+    if (this.user.token) {
+      let headers = options.headers;
+      if (headers) {
+        headers['Authorization'] = 'Token ' + this.user.token;
+      } else {
+        headers = new Headers({ Authorization: 'Token ' + this.user.token });
+      }
+      options.headers = headers;
+    }
+    return options;
+  }
+
+  private handleError(error: any) {
+    let errMsg = (error.message) ? error.message :
+        error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+
+}
