643847841e8746feb2cfdecc559884d7c23ddffb
Darrel O'Pry <darrel.opry@spry-group.com>
2017-02-17 11:07:15 -0500

    Add testing dependencies.

--- src/app/tn-common/tn-navbar/tn-navbar.component.spec.ts
+++ src/app/tn-common/tn-navbar/tn-navbar.component.spec.ts
@@ -3,6 +3,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgbModule }           from '@ng-bootstrap/ng-bootstrap';
 import { TnNavbarComponent } from './tn-navbar.component';
 
 describe('TnNavbarComponent', () => {
@@ -11,6 +13,7 @@ describe('TnNavbarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ NgbModule, RouterTestingModule.withRoutes([])],
       declarations: [ TnNavbarComponent ]
     })
     .compileComponents();
