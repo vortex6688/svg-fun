8dcbaa15d9a28b245bf8c605ad5777b0ebcfd399
Darrel O'Pry <darrel.opry@spry-group.com>
2017-02-17 20:45:07 -0500

    Implement support for scroll to fragment.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    this.route.fragment.subscribe((fragment) => {
+      if (!fragment) return;
+      const element = document.querySelector ( "#" + fragment )
+      if ( element ) element.scrollIntoView({behavior: 'smooth', block: 'end'})
+    });
+  }
+
+}
