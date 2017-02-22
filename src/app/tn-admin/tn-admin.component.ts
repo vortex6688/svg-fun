4c4219f5a0a1d1fddb6c89d14c0ab0b2a180f7dc
marton987 <martin.jfreytes@gmail.com>
2017-02-22 23:06:22 +0000

    3206 Updating tn-admin component to consider the template on the root folder

--- src/app/tn-admin/tn-admin.component.ts
+++ src/app/tn-admin/tn-admin.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'tn-admin',
   styleUrls: [ './tn-admin.component.scss' ],
-  template: '<router-outlet></router-outlet>'
+  templateUrl: './tn-admin.component.html'
 })
 export class TnAdminComponent implements OnInit {
   public ngOnInit() {
