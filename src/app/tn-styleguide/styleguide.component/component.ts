bfb51edd8a831aea8c8f07e6b84cee07495e1cd7
VORTEX <vortex6688@gmail.com>
2017-02-28 17:33:06 -0500

    put secondary nav items into a sidebar

--- src/app/tn-styleguide/styleguide.component/component.ts
+++ src/app/tn-styleguide/styleguide.component/component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'styleguide',
@@ -7,7 +9,20 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   templateUrl: './styleguide.component.html'
 })
 export class StyleguideComponent implements OnInit {
+  public root: string = '/styleguide';
+  public currentUrl: string = '';
+  public isLayout: Boolean = false;
+  public isNavbarCollapsed: Boolean = true;
+
+  constructor(private router: Router) {}
+
   public ngOnInit() {
-    console.log('StyleGuide Initialized');
+    // track current url so menu can open and close as needed.
+    this.router.events
+      .filter((event) => event instanceof NavigationEnd)
+      .subscribe((val: NavigationEnd) => {
+        this.currentUrl = val.urlAfterRedirects.replace(this.root, '');
+        this.isLayout = (this.currentUrl.indexOf('layout') !== -1);
+    });
   }
 }
