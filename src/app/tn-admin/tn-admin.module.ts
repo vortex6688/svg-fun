f1dca968115728f73fbb89471da3547399fc8cd7
Darrel O'Pry <darrel.opry@spry-group.com>
2017-03-07 16:53:39 -0500

    Move routes to main module.

--- src/app/tn-admin/tn-admin.module.ts
+++ src/app/tn-admin/tn-admin.module.ts
@@ -1,13 +1,16 @@
+// Core NG Modules (First)
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
+// Dependency Modules
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TnCommonModule } from '../tn-common';
 
 import { TnAdminComponent } from './tn-admin.component';
-import { TnAdminRoutingModule } from './tn-admin-routing.module';
+// Local Components & Services
 import { AdminNavbarComponent } from './admin-navbar';
 import { AdminOrdersListComponent } from './admin-orders-list';
 
@@ -15,6 +18,16 @@ import { AdminOrdersListComponent } from './admin-orders-list';
  * TN Module that will contain all submodules related
  * to Admin application
  */
+// Keep routes in module files to keep definitons in a single place.
+export const routes: Routes = [
+  { path: 'admin', component: TnAdminComponent, children: [
+      { path: '', redirectTo: 'orders/list', pathMatch: 'full' },
+      { path: 'orders', redirectTo: 'orders/list', pathMatch: 'full' },
+      { path: 'orders/list', component: AdminOrdersListComponent}
+    ]
+  }
+];
+
 @NgModule({
   bootstrap: [ TnAdminComponent ],
   declarations: [
@@ -26,7 +39,7 @@ import { AdminOrdersListComponent } from './admin-orders-list';
     BrowserModule,
     FormsModule,
     HttpModule,
-    TnAdminRoutingModule,
+    RouterModule.forChild(routes),
     NgbModule.forRoot(),
     TnCommonModule,
   ],
