ee2f05b989096fbfa468ef3912c85aece40d5941
Darrel O'Pry <darrel.opry@spry-group.com>
2017-03-16 15:52:23 -0400

    Properly export TnApiHttpService

--- src/app/tn-common/tn-common.module.ts
+++ src/app/tn-common/tn-common.module.ts
@@ -1,11 +1,14 @@
 import { NgModule }            from '@angular/core';
+import { HttpModule }          from '@angular/http';
 import { CommonModule }        from '@angular/common';
 import { NgbModule }           from '@ng-bootstrap/ng-bootstrap';
 import { TnNavbarComponent } from './tn-navbar/tn-navbar.component';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { TnApiHttpService } from './tn-api-http';
 
 @NgModule({
-  imports:      [ NgbModule, CommonModule, RouterModule ],
+  imports:      [ CommonModule, HttpModule, RouterModule, NgbModule ],
+  providers:    [ TnApiHttpService.provider() ],
   declarations: [ TnNavbarComponent ],
   exports:      [ CommonModule, TnNavbarComponent ]
 })
