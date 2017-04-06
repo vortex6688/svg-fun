53edd0017d0bcd4c1665e6ec74959dff6715c1e3
Darrel O'Pry <darrel.opry@spry-group.com>
2017-04-06 15:15:29 -0400

    icons as components

---
+++ src/app/tn-common/tn-icons/icon-delete.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'icon-delete',
+  template: `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="-299 391 13 11" xml:space="preserve" class="icon"><g><path class="st0" d="M-296.2,394.5v6.7c0,0.4,0.3,0.7,0.7,0.7h6c0.4,0,0.7-0.3,0.7-0.7v-6.7H-296.2z M-294.2,400.5h-0.6v-4.7h0.7v4.7H-294.2z M-292.8,400.5h-0.7v-4.7h0.7V400.5z M-291.5,400.5h-0.7v-4.7h0.7V400.5z M-290.2,400.5h-0.6v-4.7h0.7v4.7H-290.2z"/><path class="st0" d="M-288.7,392.5h-2.1v-0.8c0-0.3-0.2-0.5-0.5-0.5h-2.4c-0.3,0-0.5,0.2-0.5,0.5v0.8h-2.1c-0.3,0-0.5,0.2-0.5,0.5v0.8h8.6V393C-288.2,392.7-288.4,392.5-288.7,392.5z M-291.5,392.5h-2v-0.6h2V392.5z"/></g></svg>`,
+  styleUrls: ['./icons.scss']
+})
+export class IconDeleteComponent  {}
---
+++ src/app/tn-common/tn-icons/icon-download.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'icon-download',
+  template: `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="-299 391 13 11" xml:space="preserve" class="icon"><g><path class="st0" d="M-288.6,391h-7.8l-2.4,2.4v8.2c0,0.2,0.2,0.4,0.4,0.4h11.8c0.2,0,0.4-0.2,0.4-0.4v-8.2L-288.6,391zM-292.5,400.4l-3.9-3.1h2.4v-2.4h3.1v2.4h2.4L-292.5,400.4z M-296.9,392.6l0.8-0.8h7.2l0.8,0.8H-296.9z"/></g></svg>`,
+  styleUrls: ['./icons.scss']
+})
+export class IconDownloadComponent  {}
