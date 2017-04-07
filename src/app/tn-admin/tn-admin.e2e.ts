4ffb3905080535627fbd9f543caabd8222212798
Darrel O'Pry <darrel.opry@spry-group.com>
2017-04-07 02:55:58 -0400

    consolidate e2e tests at module level instead of component.

--- src/app/tn-admin/tn-admin.e2e.ts
+++ src/app/tn-admin/tn-admin.e2e.ts
@@ -2,23 +2,51 @@ import { browser, by, element } from 'protractor';
 
 describe('Admin', () => {
 
-  beforeEach(() => {
+  beforeAll(() => {
     browser.get('/admin');
+    // do we need to wipe localstorage to ensure we do not have a user
+    // session.
   });
 
   it('should have a title', () => {
-    let request = browser.getTitle();
-    return request.then((subject) => {
+    return browser.getTitle().then((subject) => {
       let result  = 'Type Network';
       expect(subject).toEqual(result);
     });
   });
 
   it('should have <admin>', () => {
-    let request = element(by.css('admin')).isPresent();
-    request.then((subject) => {
+    return element(by.css('admin')).isPresent().then((subject) => {
       let result  = true;
       expect(subject).toEqual(result);
     });
+  });
+
+  it('should have <admin-navbar>', () => {
+    return element(by.css('admin-navbar')).isPresent().then((subject) => {
+      let result  = true;
+      expect(subject).toEqual(result);
+    });
+  });
+
+  it('logging in with invalid credentials displays an error', () => {
+    // we should start out unauthenticated.
+    let loginModalButton = element(by.css('admin-navbar .login'));
+    expect(loginModalButton).toBeTruthy();
+    loginModalButton.click();
+
+    let loginModal = element(by.css('app-login'));
+    expect(loginModal).toBeTruthy();
+
+    loginModal.element(by.name);
+
+    let email = loginModal.element(by.name('username'));
+    email.sendKeys('incorrect@email.com');
+    let password = loginModal.element(by.name('password'));
+    password.sendKeys('incorrectPassword');
+
+    let submit = loginModal.element(by.css('a.btn.btn-success'));
+    submit.click();
+    expect(element(by.css('error'))).toBeTruthy();
   });
 });
