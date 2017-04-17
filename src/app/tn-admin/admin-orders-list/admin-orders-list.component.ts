f65943e2b762d10d2b39a6aaa4ce406446004bbe
marton987 <martin.jfreytes@gmail.com>
2017-04-17 18:01:07 -0500

    3391 Updating docs and adding title module

--- src/app/tn-admin/admin-orders-list/admin-orders-list.component.ts
+++ src/app/tn-admin/admin-orders-list/admin-orders-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Order } from '../../tn-common/orders/order.model';
+import { OrderService } from '../../tn-common/orders/order.service';
+import { AuthService } from '../../tn-common/auth/auth.service';
 
 @Component({
   selector: 'admin-orders-list',
@@ -6,19 +9,127 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-orders-list.component.scss']
 })
 export class AdminOrdersListComponent {
+
+  /**
+   * Specific number of the order to search.
+   *
+   * @public
+   * @type {string}
+   * @memberOf AdminOrdersListComponent
+   */
   public searchOrderNumber: string = '';
+
+  /**
+   * Search all the orders from a specific date.
+   *
+   * @public
+   * @type {Date}
+   * @memberOf AdminOrdersListComponent
+   */
   public searchDateStart: Date = null;
+
+  /**
+   * Search all the orders to a specific date.
+   *
+   * @public
+   * @type {Date}
+   * @memberOf AdminOrdersListComponent
+   */
   public searchDateEnd: Date = null;
+
+  /**
+   * Search all the orders related to an specific customer
+   *
+   * @public
+   * @type {string}
+   * @memberOf AdminOrdersListComponent
+   */
   public searchCustomer: string = '';
+
+  /**
+   * Search all the orders related to an specific project name
+   *
+   * @public
+   * @type {string}
+   * @memberOf AdminOrdersListComponent
+   */
   public searchProject: string = '';
+
+  /**
+   * Search all the orders related to an specific font name
+   *
+   * @public
+   * @type {string}
+   * @memberOf AdminOrdersListComponent
+   */
   public searchFont: string = '';
+
+  /**
+   * Search all the orders related to an specific foundry name
+   *
+   * @public
+   * @type {string}
+   * @memberOf AdminOrdersListComponent
+   */
   public searchFoundry: string = '';
 
+  /**
+   * Filter the orders according these licenses
+   *
+   * @public
+   * @type {Array}
+   * @memberOf AdminOrdersListComponent
+   */
   public filterLicenseTypes: string[] = [];
+
+  /**
+   * Filter the orders according these statuses
+   *
+   * @public
+   * @type {Array}
+   * @memberOf AdminOrdersListComponent
+   */
   public filterStatuses: string[] = [];
 
   /**
-   * clear search related component properties.
+   * All the filtered orders
+   *
+   * @public
+   * @type {Array}
+   * @memberOf AdminOrdersListComponent
+   */
+  public filteredOrders: Order[] = [];
+
+  constructor(private orderService: OrderService, private authService: AuthService) {
+    this.searchOrders();
+  }
+
+  /**
+   * Fetch the server and get all the orders with the provided query
+   */
+  private searchOrders() {
+    let query = {
+      id: this.searchOrderNumber,
+      from: this.searchDateStart,
+      to: this.searchDateEnd,
+      customer: this.searchCustomer,
+      project: this.searchProject,
+      font: this.searchFont,
+      foundry: this.searchFoundry,
+    };
+    this.authService.login({username: 'martin.jfreytes@gmail.com', password: 'adminadmin'}).subscribe((response) => {
+      this.orderService.find(query).subscribe((response) =>{
+        this.filteredOrders = response;
+      });
+    });
+
+  }
+
+  /**
+   * Clear search related component properties.
+   *
+   * @public
+   * @memberOf AdminOrdersListComponent
    */
   public clearSearch() {
     this.searchOrderNumber = '';
@@ -31,7 +142,10 @@ export class AdminOrdersListComponent {
   }
 
   /**
-   * clear filter related component properties.
+   * Clear filter related component properties.
+   *
+   * @public
+   * @memberOf AdminOrdersListComponent
    */
   public clearFilters() {
     this.filterLicenseTypes = [];
