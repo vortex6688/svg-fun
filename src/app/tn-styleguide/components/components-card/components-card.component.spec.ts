482a7fbe00feb272cb36c833a58d6f8228c7f5cd
VORTEX <vortex6688@gmail.com>
2017-04-19 12:03:28 -0400

    fix lints and tests

--- src/app/tn-styleguide/components/components-card/components-card.component.spec.ts
+++ src/app/tn-styleguide/components/components-card/components-card.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ComponentsCardComponent } from './components-card.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgbModule }           from '@ng-bootstrap/ng-bootstrap';
 
 describe('ComponentsCardComponent', () => {
   let component: ComponentsCardComponent;
@@ -8,6 +10,7 @@ describe('ComponentsCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ NgbModule.forRoot(), RouterTestingModule.withRoutes([]) ],
       declarations: [ ComponentsCardComponent ]
     })
     .compileComponents();
