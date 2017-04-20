00a6abb8f6a8590bd83a97ff13fe6755c91cbb66
VORTEX <vortex6688@gmail.com>
2017-04-20 11:33:12 -0400

    update row order markup and styling

--- src/app/tn-admin/admin-orders-list/orders-table/order-row/order-row.component.ts
+++ src/app/tn-admin/admin-orders-list/orders-table/order-row/order-row.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'order-row',
-  templateUrl: './order-row.component.html'
+  templateUrl: './order-row.component.html',
+  styleUrls: ['./order-row.component.scss']
 })
 export class OrderRowComponent {
   @Input() public order;
--- src/app/tn-common/tn-icons/icon-right-arrow.ts
+++ src/app/tn-common/tn-icons/icon-right-arrow.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'icon-right-arrow',
-  template: `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="13" height="13">
+  template: `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="13" height="13"
+   style="transform: inherit;">
 <path d="m 6,3 5,5 -5,5 z" fill="#000"/>
 </svg>
 `,
