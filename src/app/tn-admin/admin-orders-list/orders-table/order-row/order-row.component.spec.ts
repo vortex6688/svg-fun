db0b77a228600f903c3f6a60a7730da23af3200d
Klemensas Dranseika <hello@klemensas.net>
2017-05-02 19:09:15 +0300

    Add expand/collapse

--- src/app/tn-admin/admin-orders-list/orders-table/order-row/order-row.component.spec.ts
+++ src/app/tn-admin/admin-orders-list/orders-table/order-row/order-row.component.spec.ts
@@ -3,10 +3,12 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { OrderRowComponent } from './order-row.component';
 import { OrderStatusPipe } from './order-status.pipe';
+import { BehaviorSubject } from 'rxjs/Rx';
 
 describe('OrderRowComponent', () => {
   let component: OrderRowComponent;
   let fixture: ComponentFixture<OrderRowComponent>;
+  let collapseSubject: BehaviorSubject<boolean>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,14 +20,28 @@ describe('OrderRowComponent', () => {
   }));
 
   beforeEach(() => {
+    collapseSubject = new BehaviorSubject(true);
     fixture = TestBed.createComponent(OrderRowComponent);
     component = fixture.componentInstance;
     component.order = {id: 1};
+    component.collapseSubject = collapseSubject;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle collapse on collapse events', () => {
+    expect(component.isCollapsed).toBeTruthy('Should have been collapsed initially');
+    collapseSubject.next(false);
+    expect(component.isCollapsed).toBeFalsy('Should have been expanded after event');
+  });
+
+  it('should unsubscribe on destroy', () => {
+    expect(collapseSubject.observers.length).toEqual(1, 'Should subscribe initially');
+    fixture.destroy();
+    expect(collapseSubject.observers.length).toEqual(0, 'Should unsubscribe after destruction');
   });
 
   describe('OrderStatusPipe', () => {
--- src/app/tn-admin/admin-orders-list/orders-table/order-row/order-row.component.ts
+++ src/app/tn-admin/admin-orders-list/orders-table/order-row/order-row.component.ts
@@ -1,11 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/Rx';
 
 @Component({
   selector: 'order-row',
   templateUrl: './order-row.component.html',
   styleUrls: ['./order-row.component.scss']
 })
-export class OrderRowComponent {
+export class OrderRowComponent implements OnInit, OnDestroy {
   @Input() public order;
+  @Input() public collapseSubject: BehaviorSubject<boolean>;
   public isCollapsed = true;
+  private collapseSubscription;
+
+  public ngOnInit() {
+    this.collapseSubscription = this.collapseSubject.subscribe((collapse) => {
+      this.isCollapsed = collapse;
+    });
+  }
+
+  public ngOnDestroy() {
+    this.collapseSubscription.unsubscribe();
+  }
 }
