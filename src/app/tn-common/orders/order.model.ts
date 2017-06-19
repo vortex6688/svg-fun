cc9baf42fd0562b6968f7cd92bbc1c7a39792047
Klemensas Dranseika <hello@klemensas.net>
2017-06-19 16:47:35 +0300

    Update order model, tests

--- src/app/tn-common/orders/order.model.ts
+++ src/app/tn-common/orders/order.model.ts
@@ -1,8 +1,21 @@
 /* tslint:disable:variable-name */
 /* tslint:disable:max-classes-per-file */
-import { Payments } from '../payment/payment.model';
+import { Payment } from '../payment/payment.model';
 import { License } from '../licenses';
-import { Project } from '../projects';
+import { Family } from '../families';
+
+class Licensee {
+  public first_name: string;
+  public last_name: string;
+  public address1: string;
+  public address2?: string;
+  public state?: string;
+  public city: string;
+  public zipcode: string;
+  public country: string;
+  public company?: string;
+  public vat?: string;
+}
 
 export class Order {
   public id?: number;
@@ -13,20 +26,13 @@ export class Order {
   public status: number;
   public statusName?: string;
   public created: string;
+  public licensee: Licensee;
   public licenses?: License[];
-  public licensee_first_name: string;
-  public licensee_last_name: string;
-  public licensee_street1: string;
-  public licensee_street2?: string;
-  public licensee_state?: string;
-  public licensee_city: string;
-  public licensee_zipcode: string;
-  public licensee_country: string;
-  public licensee_company?: string;
-  public licensee_vat: string;
-  public payments: Payments[];
+  public payments: Payment[];
   public order_token: string;
   public coupon?: string;
   public coupon_discount?: number;
   public upgrade_price_adjustment: number;
+  public families?: number[] | Family[];
+  public new_customer?: boolean;
 }
