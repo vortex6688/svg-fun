d97791a2b659b2d11e0c4dd5ffa476b95b7b4e4d
Klemensas Dranseika <hello@klemensas.net>
2017-06-21 20:48:56 +0300

    Fix e2e issue

--- src/app/tn-admin/tn-admin.e2e.ts
+++ src/app/tn-admin/tn-admin.e2e.ts
@@ -129,205 +129,232 @@ describe('Admin', () => {
         expect(element(by.css('order-row')).isPresent()).toBeTruthy();
       });
 
-      describe('Order list collapsing', () => {
-        it('should allow opening collapsed single orders', () => {
-          element(by.css('order-row:first-child tr:nth-child(1) .col:nth-child(1)')).click().then(() => {
-            expect(element(by.css('order-row:first-child table')).isDisplayed()).toBeTruthy();
+      describe('Order page', () => {
+        describe('list collapsing', () => {
+          it('should allow opening collapsed single orders', () => {
+            element(by.css('order-row:first-child tr:nth-child(1) .col:nth-child(1)')).click().then(() => {
+              expect(element(by.css('order-row:first-child table')).isDisplayed()).toBeTruthy();
+            });
           });
-        });
 
-        it('should allow collapsing open single orders', () => {
-          element(by.css('order-row:first-child tr:nth-child(1) .col:nth-child(1)')).click().then(() => {
-            expect(element(by.css('order-row:first-child table')).isDisplayed()).toBeFalsy();
+          it('should allow collapsing open single orders', () => {
+            element(by.css('order-row:first-child tr:nth-child(1) .col:nth-child(1)')).click().then(() => {
+              expect(element(by.css('order-row:first-child table')).isDisplayed()).toBeFalsy();
+            });
           });
-        });
 
-        it('should allow opening all collapsed orders', () => {
-          element(by.css('orders-table > div > div:last-child > div:first-child span:first-child')).click().then(() => {
-            element.all(by.css('order-row')).each((order) => {
-              expect(order.element(by.css('table')).isDisplayed()).toBeTruthy();
+          it('should allow opening all collapsed orders', () => {
+            element(by.css('orders-table > div > div:last-child > div:first-child span:first-child'))
+            .click().then(() => {
+              element.all(by.css('order-row')).each((order) => {
+                expect(order.element(by.css('table')).isDisplayed()).toBeTruthy();
+              });
+            });
+          });
+
+          it('should allow collapsing all orders', () => {
+            element(by.css('orders-table > div > div:last-child > div:first-child span:last-child'))
+            .click().then(() => {
+              element.all(by.css('order-row')).each((order) => {
+                expect(order.element(by.css('table')).isDisplayed()).toBeFalsy();
+              });
             });
           });
         });
 
-        it('should allow collapsing all orders', () => {
-          element(by.css('orders-table > div > div:last-child > div:first-child span:last-child')).click().then(() => {
-            element.all(by.css('order-row')).each((order) => {
-              expect(order.element(by.css('table')).isDisplayed()).toBeFalsy();
+        describe('list search', () => {
+          // Clear filters
+          afterEach(() => {
+            element(by.css('orders-controls .card-block:nth-child(2) span.row small a')).click();
+            element(by.css('orders-controls .card-block:nth-child(3) span.row small b')).click();
+          });
+
+          it('should be able to search for orders by id', () => {
+            const targetId = '4';
+            const orderControls = element(by.css('orders-controls'));
+            const idField = orderControls.element(by.css('input[formControlName=id]'));
+
+            element.all(by.css('order-row')).then((orders) => {
+              expect(orders.length).toBeGreaterThan(1);
+
+              idField.sendKeys(targetId);
+              // Wait for debounce
+              browser.driver.sleep(500);
+              element.all(by.css('order-row')).then((filteredOrders) => {
+                const order = filteredOrders[0].element(by.css('tr:nth-child(1) .col:nth-child(1)'));
+                expect(filteredOrders.length).toBe(1);
+                expect(order.getText()).toEqual(targetId);
+              });
             });
           });
-        });
-      });
 
-      describe('Order list search', () => {
-        // Clear filters
-        afterEach(() => {
-          element(by.css('orders-controls .card-block:nth-child(2) span.row small a')).click();
-          element(by.css('orders-controls .card-block:nth-child(3) span.row small b')).click();
-        });
+          it('should be able to filters orders by status', () => {
+            const targetStatus = ['Paid in Full', 'Pending'];
+            const orderControls = element(by.css('orders-controls'));
+            const statusFilters = orderControls.element(by.css('fieldset:last-child'));
+            statusFilters.element(by.css('li:nth-child(2) label')).click();
+            statusFilters.element(by.css('li:nth-child(3) label')).click();
 
-        it('should be able to search for orders by id', () => {
-          const targetId = '4';
-          const orderControls = element(by.css('orders-controls'));
-          const idField = orderControls.element(by.css('input[formControlName=id]'));
-
-          element.all(by.css('order-row')).then((orders) => {
-            expect(orders.length).toBeGreaterThan(1);
-
-            idField.sendKeys(targetId);
             // Wait for debounce
             browser.driver.sleep(500);
-            element.all(by.css('order-row')).then((filteredOrders) => {
-              const order = filteredOrders[0].element(by.css('tr:nth-child(1) .col:nth-child(1)'));
-              expect(filteredOrders.length).toBe(1);
-              expect(order.getText()).toEqual(targetId);
+
+            let valid = true;
+            element.all(by.css('order-row')).each((order) => {
+              return order.element(by.css('tr:nth-child(1) .col:nth-child(5)')).getText().then((status) => {
+                if (targetStatus.indexOf(status) === -1) {
+                  valid = false;
+                  return;
+                }
+              });
+            }).then(() => {
+              expect(valid).toBeTruthy();
+            });
+          });
+
+          it('should be able to search for orders by family', () => {
+            const family = 'Zócalo';
+            const orderControls = element(by.css('orders-controls'));
+            const fontField = orderControls.element(by.css('input[formcontrolname=font]'));
+            fontField.sendKeys(family);
+            // Wait for debounce
+            browser.driver.sleep(500);
+            element.all(by.css('order-row')).each((order) => {
+              expect(order.element(by.cssContainingText('tr:last-child tbody td:first-child li', family))).toBeTruthy();
             });
           });
         });
 
-        it('should be able to filters orders by status', () => {
-          const targetStatus = ['Paid in Full', 'Pending'];
-          const orderControls = element(by.css('orders-controls'));
-          const statusFilters = orderControls.element(by.css('fieldset:last-child'));
-          statusFilters.element(by.css('li:nth-child(2) label')).click();
-          statusFilters.element(by.css('li:nth-child(3) label')).click();
-
-          // Wait for debounce
-          browser.driver.sleep(500);
-
-          let valid = true;
-          element.all(by.css('order-row')).each((order) => {
-            return order.element(by.css('tr:nth-child(1) .col:nth-child(5)')).getText().then((status) => {
-              if (targetStatus.indexOf(status) === -1) {
-                valid = false;
-                return;
-              }
+        describe('list sorting', () => {
+          it('should be able to sort orders descendingly', () => {
+            const amountHeader = element(by.css('.table-sortable .col.sortable:nth-child(3)'));
+            amountHeader.click();
+            expect(amountHeader.getAttribute('class')).toContain('sort-desc');
+            let highest = Infinity;
+            let sorted = true;
+            element.all(by.css('order-row')).each((order) => {
+              return order.element(by.css('tr:nth-child(1) .col:nth-child(3)')).getText().then((amount) => {
+                const amountNo = +amount.slice(1).replace(',', '');
+                if (highest < amountNo) {
+                  sorted = false;
+                  return;
+                }
+                highest = amountNo;
+              });
+            }).then(() => {
+              expect(sorted).toBeTruthy();
             });
-          }).then(() => {
-            expect(valid).toBeTruthy();
+          });
+
+          it('should be able to sort orders ascendingly', () => {
+            const amountHeader = element(by.css('.table-sortable .col.sortable:nth-child(3)'));
+            amountHeader.click();
+            expect(amountHeader.getAttribute('class')).toContain('sort-asc');
+            let highest = 0;
+            let sorted = true;
+            element.all(by.css('order-row')).each((order) => {
+              return order.element(by.css('tr:nth-child(1) .col:nth-child(3)')).getText().then((amount) => {
+                const amountNo = +amount.slice(1).replace(',', '');
+                if (highest >  amountNo) {
+                  sorted = false;
+                  return;
+                }
+                highest = amountNo;
+              });
+            }).then(() => {
+              expect(sorted).toBeTruthy();
+            });
           });
         });
 
-        it('should be able to search for orders by family', () => {
-          const family = 'Zócalo';
-          const orderControls = element(by.css('orders-controls'));
-          const fontField = orderControls.element(by.css('input[formcontrolname=font]'));
-          fontField.sendKeys(family);
-          // Wait for debounce
-          browser.driver.sleep(500);
-          element.all(by.css('order-row')).each((order) => {
-            expect(order.element(by.cssContainingText('tr:last-child tbody td:first-child li', family))).toBeTruthy();
+        describe('Opening details modal', () => {
+          beforeAll(() => {
+            const expandOrderEditIcon = element(by.css('order-row:first-child tr:nth-child(1) .col:nth-child(1)'));
+            expandOrderEditIcon.click();
+            const expandOrderEditButton = element(by.css('order-row .table thead:first-child button'));
+            expandOrderEditButton.click();
+          });
+
+          it('order edit modal should be revealed after clicking on the expanded row with the order details', () => {
+            expect(element(by.css('app-admin-order-edit')).isDisplayed()).toBeTruthy();
+          });
+
+          it('order edit modal should have a cancel button that closes the modal', () => {
+            const closeButton = element(by.css('app-admin-order-edit .modal-action-bar .btn-info'));
+            closeButton.getText().then((text) => {
+              expect(text).toEqual('Cancel');
+            });
+            closeButton.click().then(() => {
+              expect(element(by.css('app-admin-order-edit')).isPresent()).toBeFalsy();
+            });
           });
         });
       });
 
-      describe('Order list sorting', () => {
-        it('should be able to sort orders descendingly', () => {
-          const amountHeader = element(by.css('.table-sortable .col.sortable:nth-child(3)'));
-          amountHeader.click();
-          expect(amountHeader.getAttribute('class')).toContain('sort-desc');
-          let highest = Infinity;
-          let sorted = true;
-          element.all(by.css('order-row')).each((order) => {
-            return order.element(by.css('tr:nth-child(1) .col:nth-child(3)')).getText().then((amount) => {
-              const amountNo = +amount.slice(1).replace(',', '');
-              if (highest < amountNo) {
-                sorted = false;
-                return;
-              }
-              highest = amountNo;
+      describe('Family page', () => {
+        describe('Family list sorting', () => {
+          beforeAll(() => {
+            browser.get('/admin/products/families');
+          });
+
+          it('should be able to sort families descendingly', () => {
+            const amountHeader = element(by.css('.table-sortable .col.sortable:nth-child(3)'));
+            amountHeader.click();
+            expect(amountHeader.getAttribute('class')).toContain('sort-desc');
+            let highest = Infinity;
+            let sorted = true;
+            element.all(by.css('order-row')).each((order) => {
+              return order.element(by.css('tr:nth-child(1) .col:nth-child(3)')).getText().then((amount) => {
+                const amountNo = +amount.slice(1).replace(',', '');
+                if (highest < amountNo) {
+                  sorted = false;
+                  return;
+                }
+                highest = amountNo;
+              });
+            }).then(() => {
+              expect(sorted).toBeTruthy();
             });
-          }).then(() => {
-            expect(sorted).toBeTruthy();
           });
-        });
 
-        it('should be able to sort orders ascendingly', () => {
-          const amountHeader = element(by.css('.table-sortable .col.sortable:nth-child(3)'));
-          amountHeader.click();
-          expect(amountHeader.getAttribute('class')).toContain('sort-asc');
-          let highest = 0;
-          let sorted = true;
-          element.all(by.css('order-row')).each((order) => {
-            return order.element(by.css('tr:nth-child(1) .col:nth-child(3)')).getText().then((amount) => {
-              const amountNo = +amount.slice(1).replace(',', '');
-              if (highest >  amountNo) {
-                sorted = false;
-                return;
-              }
-              highest = amountNo;
+          it('should be able to sort families ascendingly', () => {
+            const amountHeader = element(by.css('.table-sortable .col.sortable:nth-child(3)'));
+            amountHeader.click();
+            expect(amountHeader.getAttribute('class')).toContain('sort-asc');
+            let highest = 0;
+            let sorted = true;
+            element.all(by.css('family-row')).each((order) => {
+              return order.element(by.css('tr:nth-child(1) .col:nth-child(3)')).getText().then((amount) => {
+                const amountNo = +amount.slice(1).replace(',', '');
+                if (highest >  amountNo) {
+                  sorted = false;
+                  return;
+                }
+                highest = amountNo;
+              });
+            }).then(() => {
+              expect(sorted).toBeTruthy();
             });
-          }).then(() => {
-            expect(sorted).toBeTruthy();
           });
         });
-      });
 
-      describe('Opening order details modal', () => {
-        beforeAll(() => {
-          const expandOrderEditIcon = element(by.css('order-row:first-child tr:nth-child(1) .col:nth-child(1)'));
-          expandOrderEditIcon.click();
-          const expandOrderEditButton = element(by.css('order-row .table thead:first-child button'));
-          expandOrderEditButton.click();
-        });
-
-        it('order edit modal should be revealed after clicking on the expanded row with the order details', () => {
-          expect(element(by.css('app-admin-order-edit')).isDisplayed()).toBeTruthy();
-        });
-
-        it('order edit modal should have a cancel button that closes the modal', () => {
-          const closeButton = element(by.css('app-admin-order-edit .modal-action-bar .btn-info'));
-          closeButton.getText().then((text) => {
-            expect(text).toEqual('Cancel');
+        describe('Opening single family edit modal', () => {
+          beforeAll(() => {
+            const familySingleEditButton = element(by.css('.table-sortable tbody  button'));
+            familySingleEditButton.click();
           });
-          closeButton.click().then(() => {
-            expect(element(by.css('app-admin-order-edit')).isPresent()).toBeFalsy();
+
+          it('single family edit modal should be revealed after clicking the edit botton', () => {
+            expect(element(by.css('app-admin-family-single-edit')).isDisplayed()).toBeTruthy();
           });
-        });
-      });
 
-      describe('Family list sorting', () => {
-        beforeAll(() => {
-          browser.get('/admin/products/families');
-        });
-
-        it('should be able to sort families descendingly', () => {
-          const amountHeader = element(by.css('.table-sortable .col.sortable:nth-child(3)'));
-          amountHeader.click();
-          expect(amountHeader.getAttribute('class')).toContain('sort-desc');
-          let highest = Infinity;
-          let sorted = true;
-          element.all(by.css('order-row')).each((order) => {
-            return order.element(by.css('tr:nth-child(1) .col:nth-child(3)')).getText().then((amount) => {
-              const amountNo = +amount.slice(1).replace(',', '');
-              if (highest < amountNo) {
-                sorted = false;
-                return;
-              }
-              highest = amountNo;
+          it('single family edit modal should have a discard button that closes the modal', () => {
+            const closeButton = element(by.css('app-admin-family-single-edit .modal-action-bar .btn-danger'));
+            closeButton.getText().then((text) => {
+              expect(text).toEqual('Discard');
             });
-          }).then(() => {
-            expect(sorted).toBeTruthy();
-          });
-        });
-
-        it('should be able to sort families ascendingly', () => {
-          const amountHeader = element(by.css('.table-sortable .col.sortable:nth-child(3)'));
-          amountHeader.click();
-          expect(amountHeader.getAttribute('class')).toContain('sort-asc');
-          let highest = 0;
-          let sorted = true;
-          element.all(by.css('family-row')).each((order) => {
-            return order.element(by.css('tr:nth-child(1) .col:nth-child(3)')).getText().then((amount) => {
-              const amountNo = +amount.slice(1).replace(',', '');
-              if (highest >  amountNo) {
-                sorted = false;
-                return;
-              }
-              highest = amountNo;
+            closeButton.click().then(() => {
+              expect(element(by.css('app-admin-family-single-edit')).isPresent()).toBeFalsy();
             });
-          }).then(() => {
-            expect(sorted).toBeTruthy();
           });
         });
       });
@@ -377,26 +404,6 @@ describe('Admin', () => {
         });
       });
 
-      describe('Opening single family edit modal', () => {
-        beforeAll(() => {
-          const familySingleEditButton = element(by.css('.table-sortable tbody  button'));
-          familySingleEditButton.click();
-        });
-
-        it('single family edit modal should be revealed after clicking the edit botton', () => {
-          expect(element(by.css('app-admin-family-single-edit')).isDisplayed()).toBeTruthy();
-        });
-
-        it('single family edit modal should have a discard button that closes the modal', () => {
-          const closeButton = element(by.css('app-admin-family-single-edit .modal-action-bar .btn-danger'));
-          closeButton.getText().then((text) => {
-            expect(text).toEqual('Discard');
-          });
-          closeButton.click().then(() => {
-            expect(element(by.css('app-admin-family-single-edit')).isPresent()).toBeFalsy();
-          });
-        });
-      });
     });
   }
 });
