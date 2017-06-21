85d3d47c5ca448566b4c6736e67d40ea3a4fc004
VORTEX <vortex6688@gmail.com>
2017-06-21 10:57:54 -0400

    rename order edit modal component for consistency

--- src/app/tn-admin/admin-order-edit/admin-order-edit.component.ts
+++ src/app/tn-admin/admin-order-edit/admin-order-edit.component.ts
@@ -3,10 +3,10 @@ import { Store } from '@ngrx/store';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-admin-order-details',
-  templateUrl: './admin-order-details.component.html',
-  styleUrls: ['./admin-order-details.component.scss'],
+  selector: 'app-admin-order-edit',
+  templateUrl: './admin-order-edit.component.html',
+  styleUrls: ['./admin-order-edit.component.scss'],
 })
-export class AdminOrderDetailsComponent {
+export class AdminOrderEditComponent {
   constructor(public activeModal: NgbActiveModal, private modalService: NgbModal) {}
 }
---
+++ src/app/tn-admin/admin-order-edit/index.ts
@@ -0,0 +1,1 @@
+export { AdminOrderEditComponent } from './admin-order-edit.component';
