7d73938224f0f47df7548b96620bd4c8fe495965
Klemensas Dranseika <hello@klemensas.net>
2017-06-27 20:58:56 +0300

    Redirect and clear state on logout

--- src/app/tn-admin/store/index.ts
+++ src/app/tn-admin/store/index.ts
@@ -4,10 +4,17 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { StoreRegistry } from '../../tn-common/store/store.registry';
 import { moduleReducers, moduleActions, moduleEffects } from './reducers';
+import { AuthActions } from '../../tn-common/auth';
 
 const storeAssets = StoreRegistry.registerReducers(moduleReducers, moduleActions);
-export function productionReducer(state: any, action: any) {
-    return combineReducers(storeAssets.reducers)(state, action);
+const rootReducer = (state: any, action: any) => combineReducers(storeAssets.reducers)(state, action);
+
+// Reset state on logout
+export function productionReducer(state, action) {
+  if (action.type === AuthActions.LOGOUT) {
+    state = undefined;
+  }
+  return rootReducer(state, action);
 }
 
 const devTools = process.env.ENABLE_STORE_DEVTOOLS ?
