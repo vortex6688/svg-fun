b2013288964a08edcfac69d4dcc17699a3433153
VORTEX <vortex6688@gmail.com>
2017-07-12 05:16:01 -0400

    add docs folder to host the website

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var imagemin    = require('gulp-imagemin');
 gulp.task('serve', ['sass', 'index', 'images', 'fonts'], function() {
 
     browserSync.init({
-        server: "./dist"
+        server: "./docs"
     });
 
     gulp.watch("src/scss/**/*.scss", ['sass']).on('change', browserSync.reload);
@@ -20,21 +20,21 @@ gulp.task('serve', ['sass', 'index', 'images', 'fonts'], function() {
 gulp.task('sass', function() {
     return gulp.src("src/scss/*.scss")
         .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest("dist/css"))
+        .pipe(gulp.dest("docs/css"))
         .pipe(browserSync.stream());
 });
 
 // Copy index document in the production folder
 gulp.task('index', function() {
     return gulp.src("src/index.html")
-        .pipe(gulp.dest("dist/"))
+        .pipe(gulp.dest("docs/"))
         .pipe(browserSync.stream());
 });
 
 // Copy fonts in the production folder
 gulp.task('fonts', function() {
     return gulp.src("src/fonts/**/*.*")
-        .pipe(gulp.dest("dist/fonts/"))
+        .pipe(gulp.dest("docs/fonts/"))
         .pipe(browserSync.stream());
 });
 
@@ -42,7 +42,7 @@ gulp.task('fonts', function() {
 gulp.task('images', () =>
     gulp.src('src/images/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/images'))
+        .pipe(gulp.dest('docs/images'))
         .pipe(browserSync.stream())
 );
 
